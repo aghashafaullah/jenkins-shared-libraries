b74ec306885fe6496ef668ea7873f2baedac2502
Agha Shafaullah <61188382+aghashafaullah@users.noreply.github.com>
2025-10-23 04:50:38 +0500

    Add echo statement for Docker image push

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,4 +1,5 @@
 def call(String Project, String ImageTag, String docekrhubuser){
+  echo "This is the pushing the image to the DockerHub"
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]){
       sh "docker login -u ${dockerHubUser}" -p ${dockerHubPass} 
   }
