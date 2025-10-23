94c6c0e197e1de24300db54ef507d54d610b59dc
Agha Shafaullah <61188382+aghashafaullah@users.noreply.github.com>
2025-10-23 06:00:34 +0500

    Simplify Docker push command in docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -3,7 +3,5 @@ def call(String Project, String ImageTag, String dockerhubuser){
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]){
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
-  sh """
-      docker push ${dockerhubuser}/${Project}:${ImageTag}
-  """
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
