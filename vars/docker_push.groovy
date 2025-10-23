0e7ef185e41a461fa4b5b63fd15263ae49fa7a18
Agha Shafaullah <61188382+aghashafaullah@users.noreply.github.com>
2025-10-23 06:05:37 +0500

    Remove echo statement in docker_push.groovy
    Removed unnecessary echo statement before pushing the image.

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,5 +1,4 @@
 def call(String Project, String ImageTag, String dockerhubuser){
-  echo "This is the pushing the image to the DockerHub"
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]){
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
